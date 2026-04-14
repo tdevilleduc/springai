4a8b43aff42777c0a2fa479cedc20c35e4d6fda3
Thomas Devilleduc <devilleduc.thomas@gmail.com>
2026-04-14 12:09:19 +0200

    feat: initial Spring AI project setup

---
+++ src/test/java/com/tdevilleduc/springai/SpringaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tdevilleduc.springai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
