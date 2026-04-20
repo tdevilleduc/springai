237ac22297d169ea4b50066113cdf3cf19f8968e
Thomas Devilleduc <devilleduc.thomas@gmail.com>
2026-04-20 11:30:02 +0200

    chore: remove unused import

--- src/test/java/com/tdevilleduc/springai/controllers/AnthropicControllerTest.java
+++ src/test/java/com/tdevilleduc/springai/controllers/AnthropicControllerTest.java
@@ -15,8 +15,6 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import java.util.Map;
-
 import com.github.benmanes.caffeine.cache.Cache;
 import com.github.benmanes.caffeine.cache.Caffeine;
 import com.tdevilleduc.springai.exception.GlobalExceptionHandler;
